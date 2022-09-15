4e4f8f998f0d23805288b857f4ff9d425e6184d1
Emmanuel Fofeyin Ngwamunwi <emmanuel.fofeyin-ngwamunwi@mths.ca>
2022-09-15 15:49:18 +0000

    made good changes

---
+++ ICS3U-Unit1-03.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2022 Emmanuel All rights reserved
+//
+// Created by: Emmanuel
+// Created on: Sep 2022
+// ICS3U-Unit1-03.py File, Styling properly.
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!\n";
+
+    std::cout << "\nDone.\n";
+}
